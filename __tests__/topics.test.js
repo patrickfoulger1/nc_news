ebf01853d7617b059896b6d5cb7d477db9c54993
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-01-27 12:10:32 +0000

    add topics.test.js

---
+++ __tests__/topics.test.js
@@ -0,0 +1,31 @@
+const seed = require("../db/seeds/seed");
+const request = require("supertest");
+const testData = require("../db/data/test-data/index.js");
+const db = require("../db/connection.js");
+const app = require("../app/app.js");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects with slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
+
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
+});
