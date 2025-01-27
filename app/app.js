4215c3bc5cbf3ecaa1cf07e087da927ab717b479
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-01-27 11:26:30 +0000

    Add middleware to handle unknown 500 errors

--- app/app.js
+++ app/app.js
@@ -4,4 +4,9 @@ const app = express();
 
 app.get("/api", getEndpoints);
 
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.status(500).send({ message: "Internal Server Error" });
+});
+
 module.exports = app;
