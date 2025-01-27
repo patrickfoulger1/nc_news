165046d27093954b91c5c8289202af683d069c92
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-01-27 12:11:56 +0000

    GET /api/topics endpoint

--- Controllers/app.controllers.js
+++
@@ -1,9 +0,0 @@
-const endpoints = require("../endpoints.json");
-
-exports.getEndpoints = async (request, response, next) => {
-  try {
-    response.status(200).send({ endpoints });
-  } catch (error) {
-    next(error);
-  }
-};
