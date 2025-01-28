74fa6d899454259a0c0465b5c52c2438d6da2609
patrickfoulger1 <patrickfoulger1@gmail.com>
2025-01-28 12:36:01 +0000

    add checkIfValidArticleId to handle invalid ids

--- app/utils/checkIfValidArticleId.js
+++ app/utils/checkIfValidArticleId.js
@@ -1,7 +1,6 @@
 exports.checkIfValidArticleId = (article_id) => {
   //if article_id isn't numeric
   if (isNaN(article_id)) {
-    console.log("here");
     return Promise.reject({
       status: 400,
       message: `${article_id} is not a valid id`,
